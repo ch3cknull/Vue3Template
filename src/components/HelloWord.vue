0bfd37483a7d0dbecc91e0ae532253074a5a8d06
ch3cknull <ch3cknull@outlook.com>
2021-09-17 15:20:19 +0800

    feat: add component demo

---
+++ src/components/HelloWord.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+const msg = ref('hello, world')
+</script>
+
+<template>
+  <p class="title">{{ msg }}</p>
+</template>
+
+<style lang="css" scoped>
+.title {
+  @apply text-center;
+}
+</style>
